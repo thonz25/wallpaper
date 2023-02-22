33533472841d28cead0015bac71ed1e31a5984f6
Thonz <107920901+thonz25@users.noreply.github.com>
2023-02-22 21:54:09 +0900

    version 1

--- src/components/wallpaper.tsx
+++ src/components/wallpaper.tsx
@@ -4,11 +4,22 @@ import "../components/style.scss";
 
 const ScreenSaver = () => {
   const [weather, setWeather] = useState();
-  const [quote, setQuote] = useState({});
+  const [quote, setQuote] = useState({
+    text: "",
+    author: "",
+  });
+
+  interface Time {
+    hr: number;
+    min: number;
+    sec: number;
+  }
+  const [timeNow, setTimeNow] = useState<Time>({ hr: 0, min: 0, sec: 0 });
 
   useEffect(() => {
     getLocation();
     getQuote();
+    getTime();
   }, []);
 
   const getLocation = () => {
@@ -39,7 +50,7 @@ const ScreenSaver = () => {
     console.log(quote);
   };
 
-  const fetchWeather = async (latitude, longitude) => {
+  const fetchWeather = async (latitude: number, longitude: number) => {
     const API_KEY = import.meta.env.VITE_API_KEY;
 
     const response = await fetch(
@@ -50,25 +61,49 @@ const ScreenSaver = () => {
     setWeather(data);
     console.log(data);
   };
+  const getTime = () => {
+    const time = new Date();
+    const hour = (time.getHours() / 12) * 360;
+    const mins = (time.getMinutes() / 60) * 360;
+    const secs = time.getSeconds() * 6;
 
+    setTimeNow({
+      hr: hour,
+      min: mins,
+      sec: secs,
+    });
+  };
+  setTimeout(() => {
+    getTime();
+    console.log(timeNow);
+  }, 1000);
   return (
-    <>
+    <div style={{ backgroundImage: "url(../img/wedding.jpg)" }}>
       <h1>
         Mr. <span>&</span> Mrs. Tropisado
       </h1>
 
       <div className="quote-container">
         <h2>{quote.text && quote.text}</h2>
-        <p>{quote.author}</p>
+        <p>- {quote.author} -</p>
       </div>
 
-      <div className="clock">
-        <div className="hour"></div>
-        <div className="min"></div>
-        <div className="sec"></div>
-        <div className="center"></div>
+      <div className="time-container">
+        <div className="clock">
+          <div className="hands">
+            <div className="hour" style={{ rotate: `${timeNow.hr}deg` }}></div>
+            <div className="min" style={{ rotate: `${timeNow.min}deg` }}></div>
+            <div className="sec" style={{ rotate: `${timeNow.sec}deg` }}></div>
+          </div>
+
+          <div className="number"></div>
+          <div className="twelve time">12</div>
+          <div className="three time">3</div>
+          <div className="six time">6</div>
+          <div className="nine time">9</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
